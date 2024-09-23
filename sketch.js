512556eda02e4eb533212adbabd3bfe90473d63a
Hanna2505 <hanna.dechechi@escola.pr.gov.br>
2024-09-23 15:36:45 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("black");
+  fill("white")
+  strokeWeight(1)
+  ellipse(200,100,70,70)
+  ellipse(200,175,100,100)
+  ellipse(200,280,150,150)
+  fill("black")
+  circle(190,90,5)
+circle(210,90,5)
+  fill("orange")
+  triangle(200,100,200,110,180,105)
+  fill("black")
+  circle(200,170,10)
+  circle(200,140,10)
+   stroke("brown") 
+  strokeWeight(05)
+  line(150,175,120,150)
+  line(250,175,280,150)
+  
+}
